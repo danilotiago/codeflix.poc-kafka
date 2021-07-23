269d4ffdb4af9bbed31a83b91b8c52f64ee92a3f
danilotiago <programacao.desenvolvimento@gmail.com>
2021-07-23 11:02:59 -0300

    first commit

---
+++ src/test/java/app/projetaria/pockafka/PocKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package app.projetaria.pockafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
